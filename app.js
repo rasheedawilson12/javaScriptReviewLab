9e49c61597524cb973f58f12a39e9dc96e508869
rasheedawilson12 <rasheedawilson12@gmail.com>
2023-06-12 17:36:27 -0500

    Part II - Parts A, B, & C completed

--- app.js
+++ app.js
@@ -78,3 +78,44 @@ if (age >= 16) {
 } else {
   console.log("Sorry, you're too young.");
 }
+
+// II. Loops
+// Remember: USE letwhen you initialize your for loops!
+// This is GOOD: for(let i = 0; i < 100; i++)
+// This is NO GOOD: for(i = 0; i < 100; i++)
+
+// A. The basics
+//Write a loop that will print out all the numbers from 0 to 10, inclusive
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+// Write a loop that will print out all the numbers from 10 up to and including 400
+for (let i = 10; i <= 400; i++) {
+  console.log(i);
+}
+// Write a loop that will print out every third number starting with 12 and going no higher than 4000
+for (let i = 12; i <= 400; i += 3) {
+  console.log(i);
+}
+
+// B. Get even
+// Print out the numbers that are within the range of 1 - 100
+for (let i = 1; i >= 100; i++) {
+  if (i % 2 === 0) {
+    // Adjust your code to add a message next to even numbers only that says: "<-- is an even number"
+    console.log(i + "<-- is an even number");
+  }
+}
+
+// C. Give me Five
+// For the numbers 0 - 100, print out "I found a number. High five!" if the number is a multiple of five
+for (i = 0; i <= 100; i++) {
+  if (i % 5 === 0) {
+    // Add to the code from above to print out "I found a number. Three is a crowd" if the number is a multiple of three
+    console.log("I found a number " + i + ". Hi five!");
+  } else if (i % 3 === 0) {
+    console.log("I found a number " + i + ". Three is a crowd");
+  } else {
+    console.log("I found a number " + i + ".");
+  }
+}
