e865a45fe879a22717927205a01101b8516aeaf8
rasheedawilson12 <rasheedawilson12@gmail.com>
2023-06-12 17:20:56 -0500

    Part A - Q + A completed

---
+++ app.js
@@ -0,0 +1,19 @@
+// Variables and Datatypes
+// A. Q + A
+// How do we assign a value to a variable?
+// We define a variable by first using let or const and then by giving the variable a meaningful name, using the equal sign to assign value, then adding the value that is to be stored in the variable
+// EX: let num = 8;
+// How do we change the value of a variable?
+// We can change the value of a variable by stating the vairable name (without let or const), using the equal sign to reassign the value and then adding the new value that is being reassigned to the variable. This only works with variables that have been declared with let, if the vairbale is delcared with const, the reassignment will not work.
+// EX: let fruit = "apple";
+//     fruit = "orange";
+// How do we assign an existing variable to a new variable?
+// We can assign exisiing variable to new vairables by using the let or const keyword, giving the vaiable a meaningful name, using the equal sign to assign value and then adding the variable name that we would like to assign to the new variable.
+// EX: let a = 6;
+//     let b = 7;
+//     let c = a;
+// Remind me, what are declare, assign, and define?
+
+// What is pseudocoding and why should you do it?
+
+// What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
