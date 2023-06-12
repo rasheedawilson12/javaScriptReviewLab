19806e3309f062687a0996d80e6ccedb6eb7d58c
rasheedawilson12 <rasheedawilson12@gmail.com>
2023-06-12 17:23:43 -0500

    Part C - Booleans completed

--- app.js
+++ app.js
@@ -36,3 +36,23 @@ console.log(firstVariable);
 // Create a variable called yourNameand set it equal to your name as a string. Then, write an expression that takes the string "Hello, my name is " and the variable yourNameso that it returns a new string with them concatenated.
 let yourName = "Cedra";
 console.log("Hello my name is " + yourName);
+
+// C. Boolean
+// Using the provided variable definitions, replace the blanks so that all log statements print truein the console. Answers should be all be valid JS syntax and not weird things that don't make sense but happen to print trueto the console
+const a = 4;
+const b = 53;
+const c = 57;
+const d = 16;
+const e = "Kevin";
+
+console.log(a < b);
+console.log(c > d);
+console.log("Name" == "Name");
+// FOR THE NEXT TWO, USE ONLY && OR ||
+console.log(true || false);
+console.log((false && false && false && false && false) || true);
+console.log(false === false);
+console.log(e === "Kevin");
+console.log(a + b === c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
+console.log(a < a + d); // note: the answer is a simple arithmetic equation, not something "weird"
+console.log(48 == "48");
